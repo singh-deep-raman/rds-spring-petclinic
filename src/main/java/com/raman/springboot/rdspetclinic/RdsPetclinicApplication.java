aabc710f58ae7f8d4d9ea5a2c4f9d0987f2ebfb4
singh-deep-raman <raman111092@gmail.com>
2020-09-07 16:56:59 +0530

    Initial Project Add

---
+++ src/main/java/com/raman/springboot/rdspetclinic/RdsPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.raman.springboot.rdspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdsPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdsPetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raman/springboot/rdspetclinic/RdsPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raman.springboot.rdspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdsPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
