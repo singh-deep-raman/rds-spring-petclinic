857869e5c035dc4f009e11f401e46bf78c2a4653
singh-deep-raman <raman111092@gmail.com>
2020-09-09 01:47:18 +0530

    POJOs created. Closes #1

---
+++ src/main/java/com/raman/springboot/rdspetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.raman.springboot.rdspetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/raman/springboot/rdspetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.raman.springboot.rdspetclinic.model;
+
+public class Vet extends Person {
+}
