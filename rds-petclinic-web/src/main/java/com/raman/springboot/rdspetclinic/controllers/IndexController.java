97bd14128ff949bb700062b671c457c4b1bc755d
singh-deep-raman <raman111092@gmail.com>
2020-09-13 23:04:24 +0530

    Added index html page and controller for /, /index, /index.html paths

---
+++ rds-petclinic-web/src/main/java/com/raman/springboot/rdspetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.raman.springboot.rdspetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    /** It means in case of root, /, /index or /index.html call this requet mapping method */
+    @RequestMapping({"","/","index","index.html"})
+    public String index() {
+        return "index";
+    }
+}
