819230f6a6177fa8f607c67e37df842b82346ed7
singh-deep-raman <raman111092@gmail.com>
2020-09-19 01:48:12 +0530

    owner listing on view done

--- rds-petclinic-web/src/main/java/com/raman/springboot/rdspetclinic/controllers/OwnerController.java
+++ rds-petclinic-web/src/main/java/com/raman/springboot/rdspetclinic/controllers/OwnerController.java
@@ -1,15 +1,24 @@
 package com.raman.springboot.rdspetclinic.controllers;
 
+import com.raman.springboot.rdspetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     /** R--> Don't specify '/' here, it will not work */
     @RequestMapping({"", "/index", "/index.html"})
-    public String getOwnerList() {
+    public String getOwnerList(Model model) {
+        model.addAttribute("ownersList", ownerService.findAll());
         return "owners/index";
     }
 }
