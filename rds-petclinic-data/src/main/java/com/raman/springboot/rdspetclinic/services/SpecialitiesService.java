3c528b3e79d4f6f31639037b4997262ae01a165a
singh-deep-raman <raman111092@gmail.com>
2020-09-22 01:21:16 +0530

    specialities service written for speciality

---
+++ rds-petclinic-data/src/main/java/com/raman/springboot/rdspetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.raman.springboot.rdspetclinic.services;
+
+import com.raman.springboot.rdspetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
