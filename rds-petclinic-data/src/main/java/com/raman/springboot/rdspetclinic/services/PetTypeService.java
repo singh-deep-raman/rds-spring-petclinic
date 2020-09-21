72007c4c513b3e9da6de202730e5483e1a863d3b
singh-deep-raman <raman111092@gmail.com>
2020-09-22 00:40:58 +0530

    pet type data added for bootstrap, lesson 122

---
+++ rds-petclinic-data/src/main/java/com/raman/springboot/rdspetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.raman.springboot.rdspetclinic.services;
+
+import com.raman.springboot.rdspetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
--- rds-petclinic-web/src/main/java/com/raman/springboot/rdspetclinic/bootstrap/DataLoader.java
+++ rds-petclinic-web/src/main/java/com/raman/springboot/rdspetclinic/bootstrap/DataLoader.java
@@ -1,13 +1,11 @@
 package com.raman.springboot.rdspetclinic.bootstrap;
 
 import com.raman.springboot.rdspetclinic.model.Owner;
+import com.raman.springboot.rdspetclinic.model.PetType;
 import com.raman.springboot.rdspetclinic.model.Vet;
 import com.raman.springboot.rdspetclinic.services.OwnerService;
-import com.raman.springboot.rdspetclinic.services.PetService;
+import com.raman.springboot.rdspetclinic.services.PetTypeService;
 import com.raman.springboot.rdspetclinic.services.VetService;
-import com.raman.springboot.rdspetclinic.services.map.OwnerServiceMap;
-import com.raman.springboot.rdspetclinic.services.map.PetServiceMap;
-import com.raman.springboot.rdspetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,16 +14,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     /** earlier new operator was used, now we are using DI, because these services are Spring Managed Beans now
      *  due to @Service annotation */
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         /** no need to set Ids, because we have given that responsibility to Service/Dao class */
         Owner owner = new Owner();
         owner.setFirstName("Michael");
