0e20b9a80b93de8a6f95a11eae8663d0b0f9d51d
singh-deep-raman <raman111092@gmail.com>
2020-10-01 01:50:30 +0530

    @MappedSuperClass annotation explained, lesson 145

--- rds-petclinic-data/src/main/java/com/raman/springboot/rdspetclinic/model/BaseEntity.java
+++ rds-petclinic-data/src/main/java/com/raman/springboot/rdspetclinic/model/BaseEntity.java
@@ -1,5 +1,9 @@
 package com.raman.springboot.rdspetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
@@ -7,8 +11,11 @@ import java.io.Serializable;
  * That's why it has only id field
  * We are also implementing Serializable interface which is a good thing
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -19,3 +26,17 @@ public class BaseEntity implements Serializable {
         this.id = id;
     }
 }
+/**
+ * Notes
+ *      # A mapped superclass provides persistent entity state and mapping information but is not itself an entity.
+ *      # A mapped superclass, unlike an entity, does not allow querying, persisting, or relationships to the superclass.
+ *      # @MappedSuperclass annotation is used to designate a class as mapped superclass.
+ *      # All mappings annotation can be used on the root class except for @Entity.
+ *      # Also persistent relationships defined by a mapped superclass must be unidirectional.
+ *      # Mapping information may be overridden in the subclasses by using the @AttributeOverride and @AssociationOverride annotations.
+ *      # Both abstract and concrete classes may be specified as mapped superclasses.
+ *      # It is similar to table per class inheritance but no table joins or inheritance exists in data model.
+ *      # There's no table for the mapped superclass. Inheritance only exists in object model.
+ *      # The main disadvantage of using mapped superclass is that we cannot load all entities (subclasses) represented by the mapped superclass,
+ *         i.e., polymorphic queries are not possible.
+  */
