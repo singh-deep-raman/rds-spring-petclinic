b8812b9f3710673cfac5e4f5c4a9098dce133d23
singh-deep-raman <raman111092@gmail.com>
2020-10-02 22:10:18 +0530

    vets pojo converted to jpa entity, lesson 147

--- rds-petclinic-data/src/main/java/com/raman/springboot/rdspetclinic/model/Vet.java
+++ rds-petclinic-data/src/main/java/com/raman/springboot/rdspetclinic/model/Vet.java
@@ -1,10 +1,25 @@
 package com.raman.springboot.rdspetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    /**
+     * 1. One Vet can have many specialities
+     * 2. One speciality can belong to many vets
+     * 3. From 1) & 2) we conclude that relationship will be @ManyToMany
+     * 4. By default fetch type is LAZY for @ManyToMany, we want it EAGER so that it loads everything at once
+     * 5. R--> Whenever @ManyToMany relationship is there, it is must to have join table
+     */
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>(); // to avoid NPE
 
     public Set<Speciality> getSpecialities() {
