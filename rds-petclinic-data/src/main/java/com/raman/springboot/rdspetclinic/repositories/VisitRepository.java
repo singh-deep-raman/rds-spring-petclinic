7e4413d5f670c71d95d498ad4d7d5722713be135
singh-deep-raman <raman111092@gmail.com>
2020-10-03 02:10:50 +0530

    repositories added for jpa entities, lesson 149

---
+++ rds-petclinic-data/src/main/java/com/raman/springboot/rdspetclinic/repositories/VisitRepository.java
@@ -0,0 +1,13 @@
+package com.raman.springboot.rdspetclinic.repositories;
+
+import com.raman.springboot.rdspetclinic.model.Visit;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * R--> You can extend PagingAndSortingRepository which also implements CrudRepository to use some more methods
+ * R--> You can also extend JpaRepository which extends PagingAndSortingRepository interface, to use some more functionality
+ *
+ * Our example is tied to just basic CRUD operations so CrudRepository will do the trick
+ */
+public interface VisitRepository extends CrudRepository<Visit, Long> {
+}
