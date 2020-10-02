1f1e05d534432b762f89099b147592b50f412e49
singh-deep-raman <raman111092@gmail.com>
2020-10-03 04:41:08 +0530

    pet service implementation added which fetches data from jpa, lesson 153

---
+++ rds-petclinic-data/src/main/java/com/raman/springboot/rdspetclinic/services/springdatajpa/PetSpringDataJpaService.java
@@ -0,0 +1,48 @@
+package com.raman.springboot.rdspetclinic.services.springdatajpa;
+
+import com.raman.springboot.rdspetclinic.model.Pet;
+import com.raman.springboot.rdspetclinic.repositories.PetRepository;
+import com.raman.springboot.rdspetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSpringDataJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSpringDataJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
